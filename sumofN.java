b70a6b87c5cbffe72d7f8dc711f164fad03b8649
AnishMendon26 <84612061+AnishMendon26@users.noreply.github.com>
2022-10-22 22:13:46 +0530

    program to find sum of n natural numbers

---
+++ sumofN.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+ public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int sum=(n*(n+1))/2;
+   System.out.println(sum);
+ }
+}
